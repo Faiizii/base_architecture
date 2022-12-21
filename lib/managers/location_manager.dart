d4fed56090f086326bde29ef6bf7959f9e9ab5f5
faiizii <faiiziiawan033@gmail.com>
2022-12-21 16:15:52 +0500

    background permission asked

--- lib/managers/location_manager.dart
+++ lib/managers/location_manager.dart
@@ -1,12 +1,29 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class LocationManager {
   static final LocationManager _instance = LocationManager._();
   LocationManager._();
   factory LocationManager() => _instance;
 
+  //request background location permission
+  Future<bool> requestAlwaysLocationPermissions() async {
+    Map<Permission,PermissionStatus> statues = await [
+      Permission.location,
+      Permission.ignoreBatteryOptimizations,
+    ].request();
+    if(statues[Permission.location] == PermissionStatus.granted){
+      if(await Permission.locationWhenInUse.isGranted && !await Permission.locationAlways.isGranted){
+        var status = await Permission.locationAlways.request();
+        return status == PermissionStatus.granted;
+      }else{
+        return Permission.locationAlways.isGranted;
+      }
+    }
+    return false;
+  }
   Future<bool> checkLocationPermissions() async {
 
     bool isServiceEnabled = await Geolocator.isLocationServiceEnabled();
