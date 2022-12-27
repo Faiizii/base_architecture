77f9c2f67c0524a0f155ed8892de574a081002b9
faiizii <faiiziiawan033@gmail.com>
2022-12-27 18:26:00 +0500

    db manager demo code added

--- lib/database/db_config.dart
+++ lib/database/db_config.dart
@@ -2,3 +2,21 @@
 //as it is most like package on pub.dev. on second number is sqf lite.
 //get storage is not a database. so not including that in comparison.
 // get storage can be an alternative to shared preference.
+
+import 'package:base_architecture/database/test_model.dart';
+import 'package:hive/hive.dart';
+
+class DbConfig {
+
+  void saveTestModel(TestModel model) async {
+    var box = await Hive.openBox("test_models");
+    box.put(model.name, model);
+    await box.close();
+  }
+
+  Future<TestModel> getTestModel(String name) async {
+    var box = await Hive.openBox("test_models");
+    return box.get(name);
+  }
+
+}
